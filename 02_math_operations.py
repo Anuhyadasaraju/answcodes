90629e47c71628a0e2a1b310075cc46b511cc8bc
Anuhya Gopichand <anuhya@bhaskars-MacBook-Pro.local>
2024-03-12 20:51:30 +0530

    removed while loop

--- 02_math_operations.py
+++ 02_math_operations.py
@@ -3,44 +3,41 @@
 # Python program to add, subtract, multiple and divide any two numbers
 #
 # AUTHOR  : Anuhya Dasaraju
-# DATE    : 8th March, 2024
-# VERSION : 1.0
-# DATE    : 10th March 2024
-# VERSION : 1.1 - Added input validation code
-# VERSION : 1.2 - Added code for handling divide by zero error
+# VERSION  DATE             AUTHOR           CHANGES
+# -------  ----             -------          -------
+# 1.0      08th Mar, 2024   Anuhya Dasaraju  Initial version
+# 1.1      10th Mar, 2024   Anuhya Dasaraju  Added input validation code
+# 1.2      10th Mar, 2024   Anuhya Dasaraju  Added code for handling divide by zero error
+# 1.3      10th Mar, 2024   Anuhya Dasaraju  Removed while loop for Try/Except block and added the operations in else part
+#
 # ------------------------------------------------------------------------
 print("Enter 2 numbers to perform arithmetic operations")
 # Input validation code
-while True:
-    num1 = input('Enter the first number: ')
-    num2 = input('Enter the second number: ')
-
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-        break
-
-    except ValueError:
-        print('Invalid!! You need to enter a number!\n')
-
-# addition
-add = num1 + num2
-print(f"\nAddition of {num1} and {num2} is: {add}")
-
-# subtraction rounded to 2 decimal places
-rounded_subtract = round(num1 - num2, 2)
-print(f"Subtraction of {num1} and {num2} is: {rounded_subtract}")
-
-# multiplication rounded to two decimal places
-rounded_multiply = round(num1 * num2, 2)
-print(f"Multiplication of {num1} and {num2} is: {rounded_multiply}")
-
-# handling divide by zero error
-if num2 == 0:
-    print("Can't divide by zero")
-    # division rounded to two decimal places
+try:
+    num1 = float(input('Enter the first number: '))
+    num2 = float(input('Enter the second number: '))
+except ValueError:
+    print('Invalid!! You need to enter a number!\n')
 else:
-    rounded_division = round(num1 / num2, 2)
-    print(f"Division(rounded to 2 decimal places) of {num1} and {num2} is: {rounded_division}")
+    # Performing the operations if inputs are correct
+    # addition
+    add = num1 + num2
+    print(f"\nAddition : {add}")
+
+    # subtraction rounded to 2 decimal places
+    rounded_subtract = round(num1 - num2, 2)
+    print(f"Subtraction (rounded to 2 decimal places) : {rounded_subtract}")
+
+    # multiplication rounded to two decimal places
+    rounded_multiply = round(num1 * num2, 2)
+    print(f"Multiplication (rounded to 2 decimal places) : {rounded_multiply}")
+
+    # division rounded to two decimal places
+    # handling divide by zero error
+    if num2 == 0:
+        print("Can't divide by zero")
+    else:
+        rounded_division = round(num1 / num2, 2)
+        print(f"Division (rounded to 2 decimal places) : {rounded_division}")
 
 
