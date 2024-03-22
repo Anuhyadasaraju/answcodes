ebca4092dbd9d457d9cf028eba6eb0be207c8c41
Anuhya Gopichand <anuhya@bhaskars-MacBook-Pro.local>
2024-03-22 13:40:08 +0530

    Added comments for task2

--- task2.py
+++ task2.py
@@ -10,10 +10,13 @@ class PrezentTask2(BaseCase):
         print("\n----Signed In----")
 
         #Generating Fingerprint
+        print("Clicking on profile option")
         self.click(".right-nav-item.profile-link",timeout=15)
         self.click("#fingerprint-tab",timeout=10)
+        print("Clicking on Re-take fingerprint test")
         self.click("//div[contains(text(), 'Re-take fingerprint test')]",timeout=10)
         self.click("#discover",timeout=10)
+        print("Selecting the images")
         options = ['left', 'right', 'left', 'right', 'left', 'right']
         for option in options:
             print(f"Clicking on {option} image")
@@ -22,15 +25,19 @@ class PrezentTask2(BaseCase):
             else:
                 self.click_nth_visible_element("div.card-item .text-white", number=2, timeout=10)
             self.wait(1)
+        print("Clicking on next")
         self.click("#show-fingerprint-for-btn--auto",timeout=10)
+        print("Skipping the additional details")
         #Skkiping the additional details
         for i in range(6):
             self.wait_for_element_present(".skip-button", timeout=10)
             self.click(".skip-button")
             self.wait(2)
 
+        print("Clicking on View my fingerprint")
         self.wait_for_element_present(".primary-button", timeout=10)
         self.click(".primary-button")
+        print("Clicking on Back to Prezent")
         self.wait_for_element_present("//span[contains(text(), 'Back to Prezent')]",timeout=10)
         self.click("//span[contains(text(), 'Back to Prezent')]")
         print("Generated Fingerprint")
